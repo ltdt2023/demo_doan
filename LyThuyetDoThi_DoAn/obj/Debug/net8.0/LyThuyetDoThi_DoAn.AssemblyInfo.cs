813367b807f174c0e2eb6ba9c31fa35df75abb0d
Ngan Nguyen <ngannguyen@akatekhanh-2.local>
2024-04-10 09:59:51 +0700

    ngan push

--- LyThuyetDoThi_DoAn/obj/Debug/net8.0/LyThuyetDoThi_DoAn.AssemblyInfo.cs
+++ LyThuyetDoThi_DoAn/obj/Debug/net8.0/LyThuyetDoThi_DoAn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LyThuyetDoThi_DoAn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa65d7d1a4b3123ab93786a50617f831a2a4e4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a48cdb186eef82517f610f338d19379595b2d34")]
 [assembly: System.Reflection.AssemblyProductAttribute("LyThuyetDoThi_DoAn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LyThuyetDoThi_DoAn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
